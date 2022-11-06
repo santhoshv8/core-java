f3a1e4a4376e462afbcd52f24df8c1488ba5b902
santhoshv8 <santhoshv.xworkz@gmail.com>
2022-11-06 11:23:33 +0530

    commiting arrays files

---
+++ arrays/VivanthaTajHotel.java
@@ -0,0 +1,27 @@
+class VivantaTajHotel
+{
+      public static void main (String a[])
+	  {
+	      String type = "5 Star Hotel"
+		  String address = "Bengalore";
+		  String managerNames = {"Ganesh" , "Akshata" , "Sangeeta" , "Sumanth"} ;
+		  int noOfManagers = 4 ;
+		  String foodMenu [] = {"Chikan Biriyani" , "Mutton ghee Roast" , "Butter Naan" , "Panner Butter Masala" , "French Fries" , "Tanduri" , "Veg Fried Rice" , "Curd Rice"  } ;
+		  
+		  System.out.println("Welcome To VivantaTajHotel");
+		  System.out.println("The Address of VivantaTajHotel" + address) ;
+		  
+		  System.out.println("List Of Mamagers") ;
+		  for(int m = 0 ; m < managerNames.length ; m++)
+		  {
+			  
+			 System.out.println(managerNames[m] + " ");
+		  }
+		  
+		  System.out.println("List Of FoodMenu") ;
+		  for(int f = 0 ; f < foodMenu.length ; f++)
+		  {
+			  System.out.println(foodMenu[] + " ");
+		  }
+	  }
+}
